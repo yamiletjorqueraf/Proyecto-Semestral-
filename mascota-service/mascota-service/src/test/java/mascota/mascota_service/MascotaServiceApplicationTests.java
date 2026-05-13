a622298587c6b6172f591fca621b3dbb89885492
yamiletjorqueraf <ya.jorquera@duocuc.cl>
2026-05-13 11:00:22 -0400

    Add files via upload

---
+++ mascota-service/mascota-service/src/test/java/mascota/mascota_service/MascotaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mascota.mascota_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MascotaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
