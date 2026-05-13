9860d2179ca73374dd36d7c8711dc3bf9560fd0a
yamiletjorqueraf <ya.jorquera@duocuc.cl>
2026-05-13 09:34:53 -0400

    Add files via upload

---
+++ .mvn/wrapper/mascota-service/mascota-service/src/main/java/mascota_service/mascota_service/MascotaServiceApplication.java
@@ -0,0 +1,13 @@
+package mascota_service.mascota_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MascotaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MascotaServiceApplication.class, args);
+	}
+
+}
