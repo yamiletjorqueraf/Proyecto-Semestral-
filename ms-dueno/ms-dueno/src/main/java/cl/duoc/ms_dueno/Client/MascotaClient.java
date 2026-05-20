dcc3365c5820b9b614ab7f7e01e52cd1811d77f4
paulii7u7 <paulina.m.bravo@gmail.com>
2026-05-20 14:38:02 -0400

    feat: actualizar cambios

--- ms-dueno/ms-dueno/src/main/java/cl/duoc/ms_dueno/Client/MascotaClient.java
+++ ms-dueno/ms-dueno/src/main/java/cl/duoc/ms_dueno/Client/MascotaClient.java
@@ -1,8 +1,12 @@
 package cl.duoc.ms_dueno.Client;
 
-// @FeignClient(name = "ms-mascota", url = "${ms.mascota.url}")
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "ms-mascota", url = "${ms.mascota.url}")
 public interface MascotaClient {
-    // @GetMapping("/api/v1/mascota/{id}/exists")
-    // Boolean existeMascota(@PathVariable("id") Long id);
+     @GetMapping("/api/v1/mascota/{id}/exists")
+    Boolean existeMascota(@PathVariable("id") Long id);
 
 }
