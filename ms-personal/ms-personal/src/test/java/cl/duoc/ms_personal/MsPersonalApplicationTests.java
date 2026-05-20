65d1b154f8c9c3749947d4b817f8600b7798ca63
yamiletjorqueraf <ya.jorquera@duocuc.cl>
2026-05-20 13:13:03 -0400

    Add files via upload

---
+++ ms-personal/ms-personal/src/test/java/cl/duoc/ms_personal/MsPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
