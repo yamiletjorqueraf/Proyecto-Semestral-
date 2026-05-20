9d3101914ba1865cfe531d918f91ad11f9deb26a
paulii7u7 <paulina.m.bravo@gmail.com>
2026-05-20 14:41:08 -0400

    feat: actualizar cambios

--- ms-cita/ms-cita/src/main/java/cl/duoc/ms_cita/Client/MascotaClient.java
+++ ms-cita/ms-cita/src/main/java/cl/duoc/ms_cita/Client/MascotaClient.java
@@ -1,11 +1,15 @@
 package cl.duoc.ms_cita.Client;
 
-// TODO: completar cuando ms-mascota esté listo
-// @FeignClient(name = "ms-mascota", url = "${ms.mascota.url}")
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(name = "ms-mascota", url = "${ms.mascota.url}")
 
 public interface MascotaClient {
 
-    // @GetMapping("/api/v1/mascota/{id}/exists")
-    // Boolean existeMascota(@PathVariable("id") Long id);
+     @GetMapping("/api/v1/mascota/{id}/exists")
+     Boolean existeMascota(@PathVariable("id") Long id);
 
 }
--- ms-cita/ms-cita/src/main/java/cl/duoc/ms_cita/Client/PersonalClient.java
+++ ms-cita/ms-cita/src/main/java/cl/duoc/ms_cita/Client/PersonalClient.java
@@ -1,11 +1,14 @@
 package cl.duoc.ms_cita.Client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-// TODO: completar cuando ms-personal esté listo
-// @FeignClient(name = "ms-personal", url = "${ms.personal.url}")
+
+@FeignClient(name = "ms-personal", url = "${ms.personal.url}")
 
 public interface PersonalClient {
-    // @GetMapping("/api/v1/personal/{id}/exists")
-    // Boolean existePersonal(@PathVariable("id") Long id);
+     @GetMapping("/api/v1/personal/{id}/exists")
+     Boolean existePersonal(@PathVariable("id") Long id);
 
 }
