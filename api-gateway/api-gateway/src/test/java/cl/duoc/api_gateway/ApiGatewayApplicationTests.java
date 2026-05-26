26cb67db6ea19befc11e2844d590434e490b206d
yamiletjorqueraf <ya.jorquera@duocuc.cl>
2026-05-26 01:03:01 -0400

    arreglo

--- api-gateway/api-gateway/src/test/java/cl/duoc/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/api-gateway/src/test/java/cl/duoc/api_gateway/ApiGatewayApplicationTests.java
@@ -1,9 +1,8 @@
 package cl.duoc.api_gateway;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ApiGatewayApplicationTests {
 
 	@Test
