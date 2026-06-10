2e9eef0d8f8f6109cdeee8a036b173afc85b2c03
yamiletjorqueraf <ya.jorquera@duocuc.cl>
2026-06-10 13:10:07 -0400

    MicroServicio de Resultados de Examenes

---
+++ ms-resultados-examenes/ms-resultados-examenes/src/main/java/cl/duoc/ms_resultados_examenes/MsResultadosExamenesApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_resultados_examenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsResultadosExamenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsResultadosExamenesApplication.class, args);
+	}
+
+}
