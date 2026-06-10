540f91c1865fd96ba0c5146cbf29b0a3bbcafaa7
yamiletjorqueraf <ya.jorquera@duocuc.cl>
2026-06-10 13:12:33 -0400

    MicroServicio de Farmacia

---
+++ ms-farmacia/ms-farmacia/src/main/java/cl/duoc/ms_farmacia/MsFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFarmaciaApplication.class, args);
+	}
+
+}
---
+++ ms-farmacia/ms-farmacia/src/test/java/cl/duoc/ms_farmacia/MsFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
