ce7685546c74d839f5b5c298a03f061236aea18f
paulii7u7 <paulina.m.bravo@gmail.com>
2026-06-23 01:55:23 -0400

    feat: agregar docker-compose, build-all, clean-all y gateway corregido

--- build-all.bat
+++ build-all.bat
@@ -1,36 +1,32 @@
-rem $env:DOCKER_BUILDKIT=0
-rem docker rm -f $(docker ps -aq)
+rem Eliminar contenedores e imagenes existentes
 FOR /f %%i IN ('docker ps -aq') DO docker rm -f %%i
 FOR /f %%i IN ('docker images -aq') DO docker rmi -f %%i
+
+rem Compilar todos los microservicios
 cd api-gateway
-call .\mvnw clean package -DskipTests
-
-cd ../ms-cita
-call .\mvnw clean package -DskipTests
-
-cd ../ms-dueno
-call .\mvnw clean package -DskipTests
-
-cd ../ms-farmacia
-call  .\mvnw clean package -DskipTests
-
-cd ../ms-hospitalizacion
-call .\mvnw clean package -DskipTests
-
-cd ../ms-mascota
-call .\mvnw clean package -DskipTests
-
-cd ../ms-pago
-call .\mvnw clean package -DskipTests
-
-cd ../ms-personal
-call .\mvnw clean package -DskipTests
-
-cd ../ms-resultados-examenes
 call .\mvnw clean package -DskipTests
 
 cd ../ms-usuario
 call .\mvnw clean package -DskipTests
 
+cd ../ms-personal
+call .\mvnw clean package -DskipTests
+
+cd ../ms-dueno
+call .\mvnw clean package -DskipTests
+
+cd ../ms-mascota
+call .\mvnw clean package -DskipTests
+
+cd ../ms-cita
+call .\mvnw clean package -DskipTests
+
 cd ../ms-ventas
 call .\mvnw clean package -DskipTests
+
+cd ../ms-pago
+call .\mvnw clean package -DskipTests
+
+rem Volver a la raiz y levantar Docker
+cd ..
+docker-compose up --build
--- clean-all.bat
+++ clean-all.bat
@@ -1,32 +1,25 @@
 cd api-gateway
 call .\mvnw clean
 
-cd ../ms-cita
-call .\mvnw clean
-
-cd ../ms-dueno
-call .\mvnw clean
-
-cd ../ms-farmacia
-call  .\mvnw clean
-
-cd ../ms-hospitalizacion
-call .\mvnw clean
-
-cd ../ms-mascota
-call .\mvnw clean
-
-cd ../ms-pago
+cd ../ms-usuario
 call .\mvnw clean
 
 cd ../ms-personal
 call .\mvnw clean
 
-cd ../ms-resultados-examenes
+cd ../ms-dueno
 call .\mvnw clean
 
-cd ../ms-usuario
+cd ../ms-mascota
+call .\mvnw clean
+
+cd ../ms-cita
 call .\mvnw clean
 
 cd ../ms-ventas
 call .\mvnw clean
+
+cd ../ms-pago
+call .\mvnw clean
+
+cd ..
