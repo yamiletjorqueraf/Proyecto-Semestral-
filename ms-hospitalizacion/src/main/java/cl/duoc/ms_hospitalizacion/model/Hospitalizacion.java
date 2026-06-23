069939e91c7213b2b28c28da27ce886ca20e1934
yamiletjorqueraf <ya.jorquera@duocuc.cl>
2026-06-22 22:04:23 -0400

    actualizar

--- ms-hospitalizacion/src/main/java/cl/duoc/ms_hospitalizacion/model/Hospitalizacion.java
+++ ms-hospitalizacion/src/main/java/cl/duoc/ms_hospitalizacion/model/Hospitalizacion.java
@@ -1,20 +1,6 @@
 package cl.duoc.ms_hospitalizacion.model;
 
 @Entity
-@Table(name = "hospitalizacion")
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 public class Hospitalizacion {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "idHospitalizacion")
-    private Long idHospitalizacion;
-
-    private Long idMascota;
-    private Long idDueno;
-    private LocalDate fecha_inicio;
-    private LocalDate fecha_alta;
-    private String diagnostico;
 }
